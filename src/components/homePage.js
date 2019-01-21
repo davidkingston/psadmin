7261003730c2584de7d50011b3af8c0ecc4f9e97
David A. Kingston <kingston34232@gmail.com>
2019-01-21 09:41:04 -0500

    React is working.

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>Hello, world from React!</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Home;

27c3c71794c3e89dc2a87dcff1d10a56bcc0d42b
David A. Kingston <kingston34232@gmail.com>
2019-01-25 07:51:57 -0500

    Implemented the Link component.

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,8 @@ var Route = Router.Route;
 var routes = (
   <Route name="app" path="/" handler={require('./components/app')}>
     <DefaultRoute handler={require('./components/homePage')} />
-    <Route name="authors" handler={require('./components/authors/authorPage')} />
+    <Route name="authors" handler={require('./components/authors/authorPage')} /> //if no path is specified, it defaults to the name property
+    //<Route name="author" path="/author/:authorId" handler={require('./components/authors/authorPage')} />
     <Route name="about" handler={require('./components/about/aboutPage')} />
   </Route>
 );
